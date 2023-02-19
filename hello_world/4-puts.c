3c4af6b81b1b58996c2a33084bceb5cb8b3ab1c7
Your Name <you@example.com>
2023-02-19 13:04:06 -0800

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,11 +3,10 @@
  * main - Entry point
  *
  * prints a string
+ * Return: always 0
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
-	/* return 0 */
 	return (0);
 }
