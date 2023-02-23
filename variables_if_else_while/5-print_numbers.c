dcc084e8b9263670effc94eef8b9eab44d7a8ebf
Your Name <you@example.com>
2023-02-23 06:23:58 -0800

    print_numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+*
+* Return: 0
+**/
+
+int main(void)
+{
+	char NUM;
+
+	for (NUM = '0'; NUM <= '9'; NUM++)
+		putchar(NUM);
+	putchar('\n');
+
+	return (0);
+}
