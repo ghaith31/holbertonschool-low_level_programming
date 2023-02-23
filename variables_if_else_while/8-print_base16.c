ee187777b529536c37ca6b531c3998e44ca47d8a
Your Name <you@example.com>
2023-02-23 09:04:08 -0800

    print_base

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+*
+* Return: 0
+**/
+
+int main(void)
+{
+	char NUM, XX;
+
+	for (NUM = '1'; NUM <= '9'; NUM++)
+		putchar(NUM);
+	for (XX = 'a'; XX <= 'f'; XX++)
+		putchar(XX);
+	putchar('\n');
+
+	return (0);
+}
