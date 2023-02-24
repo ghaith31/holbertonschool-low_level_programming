dba49bc9d18e85bd13d85a70a63c15b765abb035
Your Name <you@example.com>
2023-02-23 16:21:43 -0800

    test

---
+++ functions_nested_loops/putchar.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+*
+* Return: 0
+**/
+
+int main(void)
+{
+        printf('_putchar\n');
+
+        return (0);
+}
