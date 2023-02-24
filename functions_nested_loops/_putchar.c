3a4d18da8925090686524877fa6b8ca911ef2d19
Your Name <you@example.com>
2023-02-23 16:49:25 -0800

    test

--- functions_nested_loops/_putchar.c
+++ functions_nested_loops/_putchar.c
@@ -1,17 +1,13 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include <unistd.h>
 
 /**
-* main - Entry point
-*
-*
-* Return: 0
-**/
-
-int main(void)
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
 {
-        printf('_putchar\n');
-
-        return (0);
+	return (write(1, &c, 1));
 }
