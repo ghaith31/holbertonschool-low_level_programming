cd8dac84a3b2108fde7389f614e85b9de732c6db
Your Name <you@example.com>
2023-02-28 04:58:50 -0800

    test

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,13 @@
 #include "main.h"
+
 /**
- *
- * print_alphabet_x10 - print_alphabet_x10(void);
- *
- * Return: Always 0
- */
+* print_alphabet_x10 - print_alphabet_x10
+*
+* Return: Always 0
+*/
+
 void print_alphabet_x10(void)
+
 {
 	char i;
 	char h;
