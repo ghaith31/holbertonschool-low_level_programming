dc6d5e78e044be2e2d7b236a940386fc46de0637
Your Name <you@example.com>
2023-03-28 05:11:15 -0700

    gcc

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,35 +1,57 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-
+#define UNUSED(x) (void)(x)
 /**
-* main - program that adds positive numbers
-* @argc: The number of command line arguments
-* @argv: An array containing the program command line arguments
-* Return: Always 0 (Success)
-*/
-
-int main(int argc, char *argv[])
+ * StringCheck - checks string
+ * @s: string to check
+ * Return: boolean
+ */
+int StringCheck(char *s)
 {
-	int i, j = 0;
-	int sum = 0;
+	int i = 0;
 
-	if (argc <= 1)
+	for (; s[i] != '\0'; i++)
 	{
-		printf("0\n");
-		return (0);
-	}
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j]; j++)
+		if (!isdigit(s[i]))
 		{
-			if (!isdigit(argv[i][j]))
+			return (0);
+		}
+	}
+	return (1);
+}
+/**
+ * main - main function
+ * @argc: argumentc
+ * @argv: vector of arguments
+ *Return: always 0
+ */
+int main(int argc, char  *argv[])
+{
+	int i;
+	int result = 0;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (StringCheck(argv[i]))
+			{
+				result += atoi(argv[i]);
+			}
+			else
 			{
 				printf("Error\n");
 				return (1);
 			}
 		}
-		sum += atoi(argv[i]);
+		printf("%d\n", result);
+		return (0);
 	}
-	printf("%d\n", sum);
-	return (0);
+	else
+	{
+		printf("%d\n", 0);
+		return (1);
+	}
+
 }
