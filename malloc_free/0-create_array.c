0e1631b56ea1b14f790d5f3c1937de20242ce2fa
Your Name <you@example.com>
2023-03-28 07:46:37 -0700

    test

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size <= 0)
 		return (NULL);
-	t = malloc(sizeof(char) * size);
+	t= malloc (sizeof(char) * size);
 	if (t == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
