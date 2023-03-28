aa11b29dace9d38e508fc2814f82e49c6682d7af
Your Name <you@example.com>
2023-03-28 04:52:55 -0700

    test

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -6,16 +6,14 @@
  * @argv: vector of arguments
  *Return: always 0
  */
-int main(int argc, char *argv[])
-
+int main(int argc, char  *argv[])
 {
-	if(3 == argc)
-
+	if (argc == 3)
 	{
-	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-	return (0);
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
 	}
 	else
-	printf("ERROR\n");
+		printf("Error\n");
 	return (1);
 }
