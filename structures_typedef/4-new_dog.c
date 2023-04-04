977df883993709a4b98294867c0c621ac624b2c8
Your Name <you@example.com>
2023-04-04 02:31:39 -0700

    test

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,1 +1,38 @@
-#
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+#include <string.h>
+
+/**
+ * new_dog - create a new dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Return: pointer to the new dog or NULL on fail
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+
+	dog = malloc(sizeof(*dog));
+	if (dog == NULL)
+	{
+		return (NULL);
+	}
+	dog->age = age;
+	dog->name = strdup(name);
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	dog->owner = strdup(owner);
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	return (dog);
+}
