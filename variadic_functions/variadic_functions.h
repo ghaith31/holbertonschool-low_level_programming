2d823b059476587a44f23f76635842da1db783d8
Your Name <you@example.com>
2023-04-06 07:49:42 -0700

    test

---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,26 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+
+
+
+
+
+
+
+/**
+ * struct op - struct for 3
+ * @c: character to check
+ * @f: function to point to
+ */
+
+typedef struct op
+{
+	char *c;
+	int (*f)(va_list a);
+} printer;
+#endif
