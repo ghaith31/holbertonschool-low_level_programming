4ac5b6cdd6c9babfa8cce62e69481e70ec5a8784
Your Name <you@example.com>
2023-04-21 15:16:01 -0700

    test

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * add_node - prints all the elements of a list_t list.
+ * @head: pointer to the first element of the list.
+ * @str: char.
+ * Return: number of nodes.
+ **/
+list_t *add_node(list_t **head, const char *str)
+{
+list_t *newNodes = (list_t *)malloc(sizeof(list_t));
+if (!newNodes)
+return (NULL);
+newNodes->str = strdup(str);
+newNodes->len = strlen(str);
+newNodes->next = *head;
+*head = newNodes;
+return (newNodes);
+}
